23b8f1eb14da078d56d6bdb405e73d16a5486454
Thomas Löffler <loeffler@spooner-web.de>
2022-02-22 10:11:03 +0100

    [TASK] Add return value to validators

--- Classes/Domain/Validator/CustomServersideValidator.php
+++ Classes/Domain/Validator/CustomServersideValidator.php
@@ -15,7 +15,7 @@ class CustomServersideValidator extends ServersideValidator
      * @param \string $validationSetting TypoScript Setting for this field
      * @return bool
      */
-    protected function validateCustom($value, $validationSetting)
+    protected function validateCustom($value, $validationSetting): bool
     {
         // check if string has string inside
         if (stristr($value, $validationSetting)) {
