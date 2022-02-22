e5ec6bbff15ded163202921cc77bf94389b7c4ab
Thomas Löffler <loeffler@spooner-web.de>
2022-02-22 10:01:47 +0100

    [TASK] Remove obsolete parameter in addTCAcolumns()

--- Configuration/TCA/Overrides/fe_users.php
+++ Configuration/TCA/Overrides/fe_users.php
@@ -2,35 +2,35 @@
 /**
  * Add new fields to fe_users table
  */
-$GLOBALS['TCA']['fe_users']['ctrl']['type'] = 'tx_extbase_type';
-$tmpFeUsersColumns = [
-    'twitter_id' => [
-        'exclude' => 1,
-        'label' => 'LLL:EXT:femanagerextended/Resources/Private/Language/locallang_db.xlf:' .
-            'tx_femanagerextended_domain_model_user.twitter_id',
-        'config' => [
-            'type' => 'input',
-            'size' => 30,
-            'eval' => 'trim',
+    $GLOBALS['TCA']['fe_users']['ctrl']['type'] = 'tx_extbase_type';
+    $tmpFeUsersColumns = [
+        'twitter_id' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:femanagerextended/Resources/Private/Language/locallang_db.xlf:' .
+                'tx_femanagerextended_domain_model_user.twitter_id',
+            'config' => [
+                'type' => 'input',
+                'size' => 30,
+                'eval' => 'trim',
+            ],
         ],
-    ],
-    'skype_id' => [
-        'exclude' => 1,
-        'label' => 'LLL:EXT:femanagerextended/Resources/Private/Language/locallang_db.xlf:' .
-            'tx_femanagerextended_domain_model_user.skype_id',
-        'config' => [
-            'type' => 'input',
-            'size' => 30,
-            'eval' => 'trim',
+        'skype_id' => [
+            'exclude' => 1,
+            'label' => 'LLL:EXT:femanagerextended/Resources/Private/Language/locallang_db.xlf:' .
+                'tx_femanagerextended_domain_model_user.skype_id',
+            'config' => [
+                'type' => 'input',
+                'size' => 30,
+                'eval' => 'trim',
+            ],
         ],
-    ],
-    'tx_extbase_type' => [
-        'config' => [
-            'type' => 'input',
-            'default' => '0',
+        'tx_extbase_type' => [
+            'config' => [
+                'type' => 'input',
+                'default' => '0',
+            ],
         ],
-    ],
-];
+    ];
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users', $tmpFeUsersColumns, true);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users', 'twitter_id, skype_id');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users', $tmpFeUsersColumns);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users', 'twitter_id, skype_id');
